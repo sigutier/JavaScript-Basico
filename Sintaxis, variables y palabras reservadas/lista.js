a8d2ff18818e92a631220ca7700fa8b560ad65cd
sigutier <slvgutierrezrodriguez@gmail.com>
2022-11-23 13:09:50 +0100

    corrección ejercicio lista

--- Sintaxis, variables y palabras reservadas/lista.js
+++ Sintaxis, variables y palabras reservadas/lista.js
@@ -1,17 +1,13 @@
-let are_developer;
-let birth_date;
-let favorite_book;
-
-my_list = [
-    "Silvia",
-    26,
-    are_developer = true,
-    birth_date = new Date(1996, 0, 23),
-    favorite_book = {
-        title: "El cuento perfecto",
-        author: "Elísabet Benavent",
-        editorial: "SUMA",
-        date: new Date(2020, 1, 20),
-        ISBN: 9788466354813
-    }
+const my_list = [
+  "Silvia",
+  26,
+  true,
+  new Date(1996, 0, 23),
+  {
+    title: "El cuento perfecto",
+    author: "Elísabet Benavent",
+    editorial: "SUMA",
+    date: new Date(2020, 1, 20),
+    ISBN: 9788466354813,
+  },
 ];
