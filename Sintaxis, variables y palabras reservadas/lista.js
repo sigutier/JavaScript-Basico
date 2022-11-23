ac4ce9bea2c7e325ff863a10f0d348f60862c52f
sigutier <slvgutierrezrodriguez@gmail.com>
2022-11-23 12:58:40 +0100

    añado ejercicio de lista

---
+++ Sintaxis, variables y palabras reservadas/lista.js
@@ -0,0 +1,17 @@
+let are_developer;
+let birth_date;
+let favorite_book;
+
+my_list = [
+    "Silvia",
+    26,
+    are_developer = true,
+    birth_date = new Date(1996, 0, 23),
+    favorite_book = {
+        title: "El cuento perfecto",
+        author: "Elísabet Benavent",
+        editorial: "SUMA",
+        date: new Date(2020, 1, 20),
+        ISBN: 9788466354813
+    }
+];
