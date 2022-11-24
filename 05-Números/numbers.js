e4624bfbd62835ca82cc06bf919ff270395c73a9
sigutier <slvgutierrezrodriguez@gmail.com>
2022-11-24 18:33:43 +0100

    añado ejercicio de numeros

---
+++ 05-Números/numbers.js
@@ -0,0 +1,13 @@
+let alturaCm = 163;
+let alturaM = 1.63;
+let pesoKg = 55.5;
+
+let redondeoArriba = Math.ceil(alturaM);
+console.log(redondeoArriba);
+
+let redondeoAbajo = Math.floor(pesoKg);
+console.log(redondeoAbajo);
+
+let maxValor = Number.MAX_VALUE;
+console.log(maxValor);
+console.log(maxValor + 1);
